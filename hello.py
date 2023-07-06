697717c5eb2073f6e72b969252d5f92aacaa8c7b
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2023-07-06 23:50:55 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
 #print('Hello World')
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
 
-number = int(input("Enter a number: "))
-result = factorial(number)
-print(f"The factorial of {number} is {result}.")
+import requests
+
+# Hyderabad WOEID (Where On Earth ID) for MetaWeather
+WOEID = 2295414
+
+# API endpoint for weather data
+URL = f'https://www.metaweather.com/api/location/{WOEID}/'
+
+# Send a GET request to the API endpoint
+response = requests.get(URL)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    data = response.json()
+    temperature = data['consolidated_weather'][0]['the_temp']
+    temperature_celsius = round(temperature, 2)
+    print(f"The current temperature in Hyderabad is {temperature_celsius}°C.")
+else:
+    print("Failed to retrieve temperature data.")
+
 
