731d196bd88d91ec5c0df71de4a9a7b96f85a50d
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2023-07-06 23:40:49 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('Hello World')
+#print('Hello World')
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = int(input("Enter a number: "))
+result = factorial(number)
+print(f"The factorial of {number} is {result}.")
+
