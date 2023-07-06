570329a1d5a310b5d7cba6372b0ddfb26a0c297d
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2023-07-06 23:18:01 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,1 @@
-#print('Hello World')
-from flask import *
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "<h1>Welcome to Jenkins Tutorials<h1/>"
-
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0'
+print('Hello World')
